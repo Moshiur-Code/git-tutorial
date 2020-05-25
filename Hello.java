157a26de9681c79ed1f028a40ad126ca5e066f60
Moshiur Rahman <moshiurrahman4195@gmail.com>
2020-05-25 13:49:54 +0600

    Added a new method called display to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
     public static void main(string[] args) {
         system.out.printIn("vs code is Awesome...");
+
+        greeting("Moshiur Rahman");
+    }
+
+    public static void greeting(string name) {
+        system.out.printIn("welcome " + name);
     }
 }
